21b9947dad1b6c7e55979b911548d1372f9590be
qianmo-DUlI <482005684@qq.com>
2026-05-28 22:35:21 +0800

    5.28提交出版dqn

--- my_cartpole_v1.py
+++ my_cartpole_v1.py
@@ -12,9 +12,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-
-env = gym.make("CartPole-v1",render_mode ="human")
-
+#使用human可以看到训练动画，关掉加快速度
+#env = gym.make("CartPole-v1",render_mode ="human")
+env = gym.make("CartPole-v1")
 
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
@@ -41,6 +41,7 @@ device = torch.device(
 
 
 #下面实现replay memory
+#
 Transition = namedtuple("Transition",("state","action","next_state","reward"))
 class ReplayMemory:
     def __init__(self, capacity):
@@ -75,8 +76,8 @@ if __name__=="__main__":
     EPSILON_START =0.9
     EPSILON_END =0.01
     EPSILON_DECAY=2500
-    TAU =0.005
     LR =3e-4
+    TARGET_UPDATE=200
 
     n_actions =env.action_space.n
     state,info =env.reset()
@@ -144,13 +145,7 @@ if __name__=="__main__":
         if len(memory) < BATCH_SIZE:
             return
         transitions = memory.sample(BATCH_SIZE)
-        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-        # detailed explanation). This converts batch-array of Transitions
-        # to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
-
-        # Compute a mask of non-final states and concatenate the batch elements
-        # (a final state would've been the one after which simulation ended)
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
                                                 batch.next_state)), device=device, dtype=torch.bool)
         non_final_next_states = torch.cat([s for s in batch.next_state
@@ -159,30 +154,24 @@ if __name__=="__main__":
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
 
-        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-        # columns of actions taken. These are the actions which would've been taken
-        # for each batch state according to policy_net
+
         state_action_values = policy_net(state_batch).gather(1, action_batch)
 
-        # Compute V(s_{t+1}) for all next states.
-        # Expected values of actions for non_final_next_states are computed based
-        # on the "older" target_net; selecting their best reward with max(1).values
-        # This is merged based on the mask, such that we'll have either the expected
-        # state value or 0 in case the state was final.
+
         next_state_values = torch.zeros(BATCH_SIZE, device=device)
         with torch.no_grad():
             next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
-        # Compute the expected Q values
+
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
-        # Compute Huber loss
+
         criterion = nn.SmoothL1Loss()
         loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
 
-        # Optimize the model
+
         optimizer.zero_grad()
         loss.backward()
-        # In-place gradient clipping
+
         torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
         optimizer.step()
 
@@ -190,10 +179,9 @@ if __name__=="__main__":
     if torch.cuda.is_available() or torch.backends.mps.is_available():
         num_episodes = 600
     else:
-        num_episodes = 50
+        num_episodes = 100
 
     for i_episode in range(num_episodes):
-        # Initialize the environment and get its state
         state, info = env.reset()
         state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
         for t in count():
@@ -206,24 +194,11 @@ if __name__=="__main__":
                 next_state = None
             else:
                 next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-            # Store the transition in memory
             memory.push(state, action, next_state, reward)
-
-            # Move to the next state
             state = next_state
-
-            # Perform one step of the optimization (on the policy network)
             optimize_model()
-
-            # Soft update of the target network's weights
-            # θ′ ← τ θ + (1 −τ )θ′
-            target_net_state_dict = target_net.state_dict()
-            policy_net_state_dict = policy_net.state_dict()
-            for key in policy_net_state_dict:
-                target_net_state_dict[key] = policy_net_state_dict[key] * TAU + target_net_state_dict[key] * (1 - TAU)
-            target_net.load_state_dict(target_net_state_dict)
-
+            if steps_done % TARGET_UPDATE ==0:
+               target_net.load_state_dict(policy_net.state_dict())
             if done:
                 episode_durations.append(t + 1)
                 plot_durations()
@@ -235,13 +210,6 @@ if __name__=="__main__":
 
     print('Complete')
     plot_durations(show_result=True)
-
-
-    # 保存训练好的模型权重
-    torch.save(policy_net.state_dict(), "cartpole_dqn_perfect.pth")
-    print("模型权重已成功保存为: cartpole_dqn_perfect.pth")
-
-    
     plt.ioff()
     plt.show()
 
